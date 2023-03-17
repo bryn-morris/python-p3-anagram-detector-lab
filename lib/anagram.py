2dd4179d058838d83b3bf415a96b7b665f79c193
Brynfor Morris <brynfor.morris@gmail.com>
2023-03-17 09:53:45 -0500

    Created method to detect anagrams

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,15 @@
-# your code goes here!
+import ipdb
+
+class Anagram:
+
+    def __init__(self, word):
+        self.word = word
+        self.matching_words = []
+    
+    def match (self, wordlist):
+
+        for eachWord in wordlist:
+            if sorted(list(eachWord)) == sorted(list(self.word)):
+                self.matching_words.append(eachWord)
+
+        return self.matching_words
